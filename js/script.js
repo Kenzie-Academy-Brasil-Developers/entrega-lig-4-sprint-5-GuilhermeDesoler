51f4a9dc390b88f16008e6765d47468c73514344
Lucas Batista <batista.lucas3098@gmail.com>
2021-10-05 17:05:03 -0300

    creation of the positioning function

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ const main = document.getElementById('main')
 
 const gameBoard = () => {
     for (let i = 0; i < 7; i++){
-        let column = document.createElement('div')
+        let column = document.createElement('section')
         column.id = `column${i}`
+        column.classList.add('column')
         
         for(let j = 0; j < 6; j++){
             let cell = document.createElement('div')
@@ -18,4 +19,32 @@ const gameBoard = () => {
 
 gameBoard()
 
+const columns = document.querySelectorAll('section')
+console.log(columns)
 
+let player = false
+
+const position = (evt) => {
+    let click = evt.target
+    console.log(evt)
+
+    if (click.className === 'cell' && click.childElementCount === 0) {
+        if (player === false) {
+            player = true
+            let circleRed =document.createElement('div')
+            circleRed.classList.add('red')
+            click.appendChild(circleRed)
+        } else if (player === true) {
+            player = false
+            let cirlceBlack =document.createElement('div')
+            cirlceBlack.classList.add('black')
+            click.appendChild(cirlceBlack)
+        }
+    } else {
+        alert("Está casa já está ocupada!!")
+    }
+}
+
+columns.forEach((torre) => {
+    torre.addEventListener("click", position);
+});
